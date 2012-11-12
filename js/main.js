9c43ce71d674680c5d2a69188a39b2bc4d09f910
Patrick Home <patrick@kratedesign.com>
2012-11-11 19:09:40 -0500

    - new styles for the overall layout

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(function() {
 	$(window).resize(function(event) {
 		console.log($(window).width());
 	});
+
+	$('.prim-nav a').click(function(event) {
+		event.preventDefault();
+		// history.pushState({path: 'foobar', '', this.href});
+		$.get(this.href, function(data) {
+			$('.blog').html(data);
+		});
+	});
 });
